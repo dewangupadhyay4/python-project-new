5b562b3ea20cd9790ec3266396d643007f7bc9b7
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-31 15:34:04 +0530

    updating templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate , login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 
-# Create your views here.
+def signup_view(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form=SignupForm()
+    return render(request,"signup.html",{'form':form})
