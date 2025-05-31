d6e9e186bb6d25b031eb705f1294142d746f1be8
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-31 16:01:39 +0530

    task completed

--- myapp/views.py
+++ myapp/views.py
@@ -13,3 +13,19 @@ def signup_view(request):
     else:
         form=SignupForm()
     return render(request,"signup.html",{'form':form})
+
+def login_view(request):
+    if request.method=='POST':
+        user=authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return redirect('task_list')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def task_list(request):
+    tasks=Task.objects.filter(user=request.user)
+    return render(request, 'task_list.html',{'tasks':tasks})
--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import mysql.connector.django
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'myapp','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
