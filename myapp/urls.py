15f02f9c3eecc7fa513c56e69f96fc79b3d07095
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-31 21:21:18 +0530

    new commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/',views.login_view,name='login'),
     path('logout/',views.logout_view,name='logout'),
     path('',views.task_list,name='task_list'),
+    path('task/create/',views.task_create,name='task_create'),
+    path('task/edit/<int:pk>/',views.task_edit,name='task_edit'),
+    path('task_delete/<int:pk>/',views.task_delete,name='task_delete'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,17 +15,57 @@ def signup_view(request):
     return render(request,"signup.html",{'form':form})
 
 def login_view(request):
-    if request.method=='POST':
-        user=authenticate(username=request.POST['username'], password=request.POST['password'])
+    error = ""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
         if user:
             login(request, user)
             return redirect('task_list')
-    return render(request, 'login.html')
+        else:
+            error = "Invalid credentials"
+    return render(request, 'login.html', {'error': error})
+
 
 def logout_view(request):
     logout(request)
     return redirect("login")
 
+@login_required
 def task_list(request):
     tasks=Task.objects.filter(user=request.user)
     return render(request, 'task_list.html',{'tasks':tasks})
+
+@login_required
+def task_create(request):
+    if request.method=='POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            task=form.save(commit=False)
+            task.user=request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form=TaskForm()
+    return render(request, 'task_form.html',{'form':form, 'title': 'cerate task'})
+
+@login_required
+def task_edit(request, pk):
+    task=Task.objects.get(pk=pk,user=request.user)
+    if request.method=='POST':
+        form=TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form=TaskForm()
+    return render(request,"task_form.html",{'form':form, 'title': 'Edit Task'})
+
+@login_required
+def task_delete(request,pk):
+    task=Task.objects.get(pk=pk,user=request.user)
+    if request.method=='POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'task_confirm_delete.html',{'task':task})
